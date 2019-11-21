7fb289b2f793336fa966d7b411e114bfcc475a99
Matthew Zelinsky <zelinsky@udel.edu>
2019-11-20 19:51:47 -0500

    Added components. Finished navbar.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SongComponent } from './song/song.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    ProfileComponent,
+    SongComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
