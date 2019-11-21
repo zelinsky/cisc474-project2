d45bd05c3c059016b459c75ad6659464cd36924e
Matthew Zelinsky <zelinsky@udel.edu>
2019-11-21 16:30:50 -0500

    Added example of getting service from api.

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  posts;
+
+  constructor(private apiService: ApiService) { }
+
+
 
   ngOnInit() {
+    this.apiService.getPosts().subscribe((data) => {
+      console.log(data);
+      this.posts = data;
+    });
   }
 
 }
