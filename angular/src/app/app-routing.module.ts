a4b6692e759c05a27d241bc1143d0c9ecbbf3980
Sophia Freaney <sfreaney@udel.edu>
2019-11-24 21:39:48 -0500

    Started post page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { PostComponent } from './post/post.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'profile', component: ProfileComponent},
+  {path: 'post', component: PostComponent},
 ];
 
 @NgModule({
