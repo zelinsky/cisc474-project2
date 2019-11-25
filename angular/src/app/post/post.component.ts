ae2d24c1781a57090ac827526c39f538cc6f5da4
Sophia Freaney <sfreaney@udel.edu>
2019-11-25 00:47:02 -0500

    changing favicon and added image folder

--- angular/src/app/post/post.component.ts
+++ angular/src/app/post/post.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  makeComment() {
+    window.alert('You have made a comment!');
+  }
+
   constructor() { }
 
   ngOnInit() {
