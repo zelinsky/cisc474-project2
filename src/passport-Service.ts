ced635aca5a74bfffeefdfa862b4120072081774
LandonJones <lhjones@udel.edu>
2019-11-27 15:31:37 -0500

    added auth, registering and login

---
+++ src/passport-Service.ts
@@ -0,0 +1,22 @@
+import { doesNotReject } from "assert";
+
+const passport = require('passport'); 
+const JwtStrategy = require('passport-jwt').Strategy; 
+const ExtractJwt = require('passport-jwt').ExtractJwt; 
+
+const JwtOptions = { 
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
+    secretOrKey: "SuperSecretKeyzz" 
+};
+const JwtLogin = new JwtStrategy(JwtOptions, function(payload: Object, callback: any){
+    
+    callback(null, payload); 
+    
+});
+passport.use(JwtLogin);
+ 
+export class PassportService{ 
+    
+    public requireAuth = passport.authenticate('jwt', { session: false });  
+
+}
