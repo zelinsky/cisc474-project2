f063dc3ff891683e8719608651f53aece0717a09
Matthew Zelinsky <zelinsky@udel.edu>
2019-12-01 18:10:33 -0500

    Started to implement new post form.:

--- angular/src/app/api.service.ts
+++ angular/src/app/api.service.ts
@@ -25,4 +25,7 @@ export class ApiService {
   public postSong(song: object) {
     return this.httpClient.post(`${this.API_URL}/api/songs`, song);
   }
+  public postPost(songId: string, post: object) {
+    return this.httpClient.post(`${this.API_URL}/api/songs/${songId}/posts`, post);
+  }
 }
