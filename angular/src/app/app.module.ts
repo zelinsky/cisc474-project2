be16c1509d7072fdc02c431756f30d3ccc859615
Joshua Davis <jhdavis@udel.edu>
2019-12-02 16:05:59 -0500

    Add material design includes

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PostsViewComponent } from './posts-view/posts-view.component';
 import { PostComponent } from './post/post.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NewSongComponent } from './new-song/new-song.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { NewSongComponent } from './new-song/new-song.component';
     HttpClientModule,
     CommonModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
